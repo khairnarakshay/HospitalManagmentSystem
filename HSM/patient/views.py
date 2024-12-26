5f9928fd5a194a29bacce594eee698d5fcc2f39b
Akshay Khairnar <khairnarakshay1722@gmail.com>
2024-12-26 12:00:38 +0530

    views for login and registration

--- HSM/patient/views.py
+++ HSM/patient/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+
 
 # Create your views here.
+from django.shortcuts import render
+#
+# Create your views here.
+def login(request):
+    return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'registration.html')
